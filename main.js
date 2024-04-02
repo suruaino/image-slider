8abffa6412507e513a8c0a51cc9b8b9f2ef1c737
SURU EMMANUEL <suruaino@gmail.com>
2024-04-02 19:15:15 +0100

    added indicator click event

--- main.js
+++ main.js
@@ -1,29 +1,72 @@
 let currentSlide = 0;
 const slides = document.querySelectorAll('.slider-box img');
 const totalSlides = slides.length;
-console.log(totalSlides);
 const backBtn = document.querySelector('.back-btn');
 const nextBtn = document.querySelector('.next-btn');
 const indicators = document.querySelectorAll('.slider-indicator');
-console.log(indicators.length);
 
+document.addEventListener("DOMContentLoaded", function () {
+    indicators[0].classList.add('active');
 
-nextBtn.addEventListener('click', () => {
-    if (currentSlide < totalSlides - 1) {
-        currentSlide++;
-        const translateValue = -currentSlide * 105;
-        slides.forEach((slide) => {
-            slide.style.transform = `translateX(${translateValue}%)`;
+    nextBtn.addEventListener('click', () => {
+        if (currentSlide < totalSlides - 1) {
+            currentSlide++;
+            const translateValue = -currentSlide * 105;
+            slides.forEach((slide) => {
+                slide.style.transform = `translateX(${translateValue}%)`;
+            });
+        }
+    
+        indicators.forEach((indicator, i) => {
+            if (i === currentSlide) {
+              indicator.classList.add('active');
+              indicators[0].classList.remove('active');
+            } else {
+              indicator.classList.remove('active');
+            }
+          });
+      })
+    
+    backBtn.addEventListener('click', () => {
+        if (currentSlide > 0) {
+            currentSlide--;
+            const translateValue = -currentSlide * 105;
+            slides.forEach((slide) => {
+                slide.style.transform = `translateX(${translateValue}%)`;
+            });
+        }
+    
+        indicators.forEach((indicator, i) => {
+            if (i === currentSlide) {
+              indicator.classList.add('active');
+            } else {
+              indicator.classList.remove('active');
+            }
+          });
+    })
+
+      indicators.forEach((indicator, i) => {
+        indicator.addEventListener('click', () => {
+            console.log(`${i} is clicked!`)
+            if (currentSlide < slides.length ) {
+                const translateValue = -i * 105;
+                slides.forEach((slide) => {
+                    slide.style.transform = `translateX(${translateValue}%)`;
+                    
+                });
+            }
+
+            if (i === currentSlide) {
+                indicator.classList.add('active');
+              } else {
+                indicator.classList.remove('active');
+            }
+        
+                
+    
+
         });
-    }
-  })
+    });
+    
 
-backBtn.addEventListener('click', () => {
-    if (currentSlide > 0) {
-        currentSlide--;
-        const translateValue = -currentSlide * 105;
-        slides.forEach((slide) => {
-            slide.style.transform = `translateX(${translateValue}%)`;
-        });
-    }
-})
+});
